e8b863b9fef16086a16f2bcef2745add41444301
Clone888 <dwclone@gmail.com>
2023-09-22 11:02:08 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿for (int hour = 0; hour < 24; hour++)
+{
+    for(int min = 0; min < 60; min++)
+    {
+        for (int sek = 0; sek < 60; sek++)
+        {
+            Console.WriteLine(time(hour) + ":" + time(min) + ":" + time(sek));
+        }
+
+    }
+}
+
+
+
+string time(int nummer)
+{
+    if (nummer <= 9)
+    {
+        return "0" + nummer;
+    }
+    else return nummer.ToString();
+}
